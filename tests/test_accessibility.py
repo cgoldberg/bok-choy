d9855f4a22fee87a6f9e94c85c2de131dbbdf9e7
Jesse Zoldak <zoldak@edx.org>
2015-04-30 11:33:45 -0400

    temp commit

--- tests/test_accessibility.py
+++ tests/test_accessibility.py
@@ -3,9 +3,13 @@ Test accessibility reporting.
 """
 import json
 import requests
+from textwrap import dedent
 
 from bok_choy.web_app_test import WebAppTest
 from .pages import VisiblePage
+
+AXS_URL = "https://raw.githubusercontent.com/GoogleChrome/accessibility-developer-tools/stable/dist/js/axs_testing.js"
+AXS_FILE = 'tests/axs_testing.js'
 
 class AccessibilityTest(WebAppTest):
     """
@@ -13,38 +17,36 @@ class AccessibilityTest(WebAppTest):
     """
     def setUp(self):
         super(AccessibilityTest, self).setUp()
-
-    def test_visible(self):
-        # (Pdb) self.browser.service.service_url
-        # 'http://localhost:33225/wd/hub'
-        # figure out what session
-        # see https://groups.google.com/forum/#!topic/phantomjs/tW2p3dlJ9Gw
         self.page = VisiblePage(self.browser).visit()
+
+    def test_phantom_execute(self):
+        # Get the session_id from ghostdriver so that we can inject JS into the page
+        # The ghostdriver URL will be something like this: 'http://localhost:33225/wd/hub'
+        self.assertTrue(self.page.is_visible('superman'))
         ghostdriver_url = self.browser.service.service_url
         resp = requests.get('{}/sessions'.format(ghostdriver_url))
         sessions = resp.json()
         value = sessions.get('value')
         session_id = value[0].get('id')
-        print session_id
 
-        # OK this works
-        resp = requests.get('{}/session/{}/title'.format(ghostdriver_url, session_id))
-        print resp.text
-
-        # this works too
-        resp = requests.get('{}/session/{}/url'.format(ghostdriver_url, session_id))
-        print resp.text
-
-        # payload = {"url": "http://localhost:8003/selector.html"}
-        # resp = requests.post('{}/session/{}/url'.format(ghostdriver_url, session_id), data=json.dumps(payload))
-        # print resp.text
-
-        payload = {"script": "return arguments[0] + arguments[0]", "args": [1]}
+        script = dedent("""
+            var page = this;
+            return page.injectJs(arguments[0]);
+        """)
+        payload = {"script": script, "args": [AXS_FILE]}
         resp = requests.post('{}/session/{}/phantom/execute'.format(ghostdriver_url, session_id), data=json.dumps(payload))
-        print resp.text
+        self.assertEqual(True, resp.json().get('value'))
 
-        self.assertEqual(2, resp.json().get('value'))
-        # from nose.tools import set_trace; set_trace()
+        script = dedent("""
+            var page = this;
+            page.injectJs(arguments[0]);
+            var report = page.evaluate(function() {
+              var results = axs.Audit.run();
+              return results;
+            });
+            return report;
+        """)
+        payload = {"script": script, "args": [AXS_FILE]}
+        resp = requests.post('{}/session/{}/phantom/execute'.format(ghostdriver_url, session_id), data=json.dumps(payload))
+        report = resp.json().get('value')
 
-
-        self.assertTrue(self.page.is_visible('superman'))
